2a7b885aa2fe4589d3bacfbaa6a9a1856e99eb6e
Memelicious36 <marjoriembengeni5@gmail.com>
2023-02-16 13:53:45 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include  < stdio.h >
+/**
+ * main -Print a line of code using puts
+ *
+ * Return: 0
+*/
+
+Int main(void)
+{
+	Puts("\"Programming is like building a multilingual puzzle ");
+	return (0);
+}
